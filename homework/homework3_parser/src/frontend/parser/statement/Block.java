ee4ae4207d38ed01f79ad47bde6e46bf1d6bbf6f
YJK <yjk_official@foxmail.com>
2022-10-03 10:20:27 +0800

    feat: complete Block class in statement package

---
+++ homework/homework3_parser/src/frontend/parser/statement/Block.java
@@ -0,0 +1,36 @@
+package frontend.parser.statement;
+
+import frontend.lexer.Token;
+import frontend.parser.SyntaxNode;
+import frontend.parser.statement.blockitem.BlockItem;
+
+import java.util.ArrayList;
+
+public class Block implements SyntaxNode {
+    private final String name = "<Block>";
+    private Token leftBrace; // '{'
+    private ArrayList<BlockItem> blockItems;
+    private Token rightBrace; // '}'
+
+    public Block(Token leftBrace,
+                 ArrayList<BlockItem> blockItems,
+                 Token rightBrace) {
+        this.leftBrace = leftBrace;
+        this.blockItems = blockItems;
+        this.rightBrace = rightBrace;
+    }
+
+    @Override
+    public String syntaxOutput() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.leftBrace.syntaxOutput());
+        if (blockItems != null && blockItems.size() != 0) {
+            int len = blockItems.size();
+            for (int i = 0; i < len; i++) {
+                sb.append(this.blockItems.get(i).syntaxOutput());
+            }
+        }
+        sb.append(this.rightBrace.syntaxOutput());
+        return sb.toString();
+    }
+}
