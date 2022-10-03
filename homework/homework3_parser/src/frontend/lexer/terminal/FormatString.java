961b2bf2ae874c6618e6e90c77c11290e567a980
YJK <yjk_official@foxmail.com>
2022-10-03 10:51:19 +0800

    feat: complete StmtEle interface and StmtAssign class in statement package

---
+++ homework/homework3_parser/src/frontend/lexer/terminal/FormatString.java
@@ -0,0 +1,23 @@
+package frontend.lexer.terminal;
+
+import frontend.lexer.Token;
+import frontend.lexer.TokenType;
+import frontend.parser.SyntaxNode;
+
+/**
+ * 格式字符串终结符 FormatString
+ */
+public class FormatString implements SyntaxNode {
+    private Token token; // STRCOON
+
+    public FormatString(String str, int lineNum) {
+        this.token = new Token(TokenType.STRCON, lineNum, str);
+    }
+
+    @Override
+    public String syntaxOutput() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.token.syntaxOutput());
+        return sb.toString();
+    }
+}
