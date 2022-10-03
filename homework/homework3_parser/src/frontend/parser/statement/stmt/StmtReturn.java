0a623fb8e92c3148899cbc926bc2bb049f459054
YJK <yjk_official@foxmail.com>
2022-10-03 14:48:19 +0800

    feat: complete StmtReturn class in statement package

---
+++ homework/homework3_parser/src/frontend/parser/statement/stmt/StmtReturn.java
@@ -0,0 +1,29 @@
+package frontend.parser.statement.stmt;
+
+import frontend.lexer.Token;
+import frontend.parser.expression.Exp;
+
+public class StmtReturn implements StmtEle {
+    private Token returnTk; // 'return'
+    private Exp exp; // MAY exist
+    private Token semicn; // ';'
+
+    public StmtReturn(Token returnTk,
+                      Exp exp,
+                      Token semicn) {
+        this.returnTk = returnTk;
+        this.exp = exp;
+        this.semicn = semicn;
+    }
+
+    @Override
+    public String syntaxOutput() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.returnTk.syntaxOutput());
+        if (exp != null) {
+            sb.append(this.exp.syntaxOutput());
+        }
+        sb.append(this.semicn.syntaxOutput());
+        return sb.toString();
+    }
+}
