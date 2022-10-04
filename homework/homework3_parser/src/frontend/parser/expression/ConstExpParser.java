0cb7e31920092e308e93a77f0efce0a57f624863
YJK <yjk_official@foxmail.com>
2022-10-04 17:15:54 +0800

    feat: complete ConstDefParser

--- homework/homework3_parser/src/frontend/parser/expression/ConstExpParser.java
+++ homework/homework3_parser/src/frontend/parser/expression/ConstExpParser.java
@@ -1,12 +1,18 @@
 package frontend.parser.expression;
 
 import frontend.lexer.TokenListIterator;
+import frontend.parser.expression.multiexp.AddExp;
+import frontend.parser.expression.multiexp.AddExpParser;
 
 public class ConstExpParser {
     private TokenListIterator iterator;
     /* ConstExp Attributes */
 
+
     public ConstExp parseConstExp() {
-        return null;
+        AddExpParser addExpParser = new AddExpParser(this.iterator);
+        AddExp addExp = addExpParser.parseAddExp();
+        ConstExp constExp = new ConstExp(addExp);
+        return constExp;
     }
 }
---
+++ homework/homework3_parser/src/frontend/parser/expression/multiexp/AddExpParser.java
@@ -0,0 +1,16 @@
+package frontend.parser.expression.multiexp;
+
+import frontend.lexer.TokenListIterator;
+
+public class AddExpParser {
+    private TokenListIterator iterator;
+
+    public AddExpParser(TokenListIterator iterator) {
+        this.iterator = iterator;
+    }
+
+    public AddExp parseAddExp() {
+        /* TODO */
+        return null;
+    }
+}
