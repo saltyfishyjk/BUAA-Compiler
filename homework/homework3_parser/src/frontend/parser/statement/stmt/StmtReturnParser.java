f54804793f0e763c2df4d7c75816caa2c434dc2a
YJK <yjk_official@foxmail.com>
2022-10-05 16:25:15 +0800

    feat: complete StmtReturnParser

--- homework/homework3_parser/src/frontend/parser/statement/stmt/StmtReturnParser.java
+++ homework/homework3_parser/src/frontend/parser/statement/stmt/StmtReturnParser.java
@@ -1,16 +1,31 @@
 package frontend.parser.statement.stmt;
 
+import frontend.lexer.Token;
 import frontend.lexer.TokenListIterator;
+import frontend.lexer.TokenType;
+import frontend.parser.expression.Exp;
+import frontend.parser.expression.ExpParser;
 
 public class StmtReturnParser {
     private TokenListIterator iterator;
+    /* StmtPrint Attributes */
+    private Token returnTk; // 'return'
+    private Exp exp;
+    private Token semicn; // ';'
 
     public StmtReturnParser(TokenListIterator iterator) {
         this.iterator = iterator;
     }
 
     public StmtReturn parseStmtReturn() {
-        /* TODO */
-        return null;
+        this.returnTk = this.iterator.readNextToken();
+        if (!this.returnTk.getType().equals(TokenType.RETURNTK)) {
+            System.out.println("EXPECT RETURNTK IN STMTRETURNPARSER");
+        }
+        ExpParser expParser = new ExpParser(this.iterator);
+        this.exp = expParser.parseExp();
+        this.semicn = this.iterator.readNextToken();
+        StmtReturn stmtReturn = new StmtReturn(this.returnTk, this.exp, this.semicn);
+        return stmtReturn;
     }
 }
