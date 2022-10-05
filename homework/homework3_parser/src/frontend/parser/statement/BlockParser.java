f4652b978baf9f124b44cdfccc184e065bf3d292
YJK <yjk_official@foxmail.com>
2022-10-05 11:11:00 +0800

    feat: complete MainFuncDefParser

---
+++ homework/homework3_parser/src/frontend/parser/statement/BlockParser.java
@@ -0,0 +1,16 @@
+package frontend.parser.statement;
+
+import frontend.lexer.TokenListIterator;
+
+public class BlockParser {
+    private TokenListIterator iterator;
+
+    public BlockParser(TokenListIterator iterator) {
+        this.iterator = iterator;
+    }
+
+    public Block parseBlock() {
+        /* TODO */
+        return null;
+    }
+}
