103c70b4529bc135dd9e4bf9064fa38d4c9fe382
YJK <yjk_official@foxmail.com>
2022-10-05 12:19:25 +0800

    feat: complete BlockItemParser

--- homework/homework3_parser/src/frontend/parser/statement/StmtParser.java
+++
@@ -1,4 +0,0 @@
-package frontend.parser.statement;
-
-public class StmtParser {
-}
---
+++ homework/homework3_parser/src/frontend/parser/statement/stmt/StmtParser.java
@@ -0,0 +1,16 @@
+package frontend.parser.statement.stmt;
+
+import frontend.lexer.TokenListIterator;
+
+public class StmtParser {
+    private TokenListIterator iterator;
+
+    public StmtParser(TokenListIterator iterator) {
+        this.iterator = iterator;
+    }
+
+    public Stmt parseStmt() {
+        /* TODO */
+        return null;
+    }
+}
