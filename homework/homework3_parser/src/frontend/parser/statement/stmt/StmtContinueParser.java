e635b25ea04026fbf593a47ff87c6873a61923ce
YJK <yjk_official@foxmail.com>
2022-10-05 16:01:20 +0800

    feat: complete StmtContinueParser

--- homework/homework3_parser/src/frontend/parser/statement/stmt/StmtContinueParser.java
+++ homework/homework3_parser/src/frontend/parser/statement/stmt/StmtContinueParser.java
@@ -1,16 +1,29 @@
 package frontend.parser.statement.stmt;
 
+import frontend.lexer.Token;
 import frontend.lexer.TokenListIterator;
+import frontend.lexer.TokenType;
 
 public class StmtContinueParser {
     private TokenListIterator iterator;
+    /* StmtContinue Attributes */
+    private Token continueTk; // 'continue'
+    private Token semicn; // ';'
 
     public StmtContinueParser(TokenListIterator iterator) {
         this.iterator = iterator;
     }
 
     public StmtContinue parseStmtContinue() {
-        /* TODO */
-        return null;
+        this.continueTk = this.iterator.readNextToken();
+        if (!this.continueTk.getType().equals(TokenType.CONTINUETK)) {
+            System.out.println("EXPECT CONTINUETK IN STMTCONTINUEPARSER");
+        }
+        this.semicn = this.iterator.readNextToken();
+        if (!this.semicn.getType().equals(TokenType.SEMICN)) {
+            System.out.println("EXPECT SEMICN IN STMTCONTINUEPARSER");
+        }
+        StmtContinue stmtContinue = new StmtContinue(this.continueTk, this.semicn);
+        return stmtContinue;
     }
 }
