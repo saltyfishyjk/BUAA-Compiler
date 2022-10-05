c5c2f0b1812fdd2fcde45fb984147b59adf7d684
YJK <yjk_official@foxmail.com>
2022-10-05 16:22:33 +0800

    feat: complete StmtPrintParser

--- homework/homework3_parser/src/frontend/parser/terminal/FormatString.java
+++ homework/homework3_parser/src/frontend/parser/terminal/FormatString.java
@@ -8,7 +8,11 @@ import frontend.parser.SyntaxNode;
  * 格式字符串终结符 FormatString
  */
 public class FormatString implements SyntaxNode {
-    private Token token; // STRCOON
+    private Token token; // STRCON
+
+    public FormatString(Token token) {
+        this.token = token;
+    }
 
     public FormatString(String str, int lineNum) {
         this.token = new Token(TokenType.STRCON, lineNum, str);
