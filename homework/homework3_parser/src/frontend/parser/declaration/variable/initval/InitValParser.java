dd00dc63900c44888f9907c66c3a56dc3a51f5e0
YJK <yjk_official@foxmail.com>
2022-10-05 10:03:41 +0800

    feat: complete VarDefParser

---
+++ homework/homework3_parser/src/frontend/parser/declaration/variable/initval/InitValParser.java
@@ -0,0 +1,16 @@
+package frontend.parser.declaration.variable.initval;
+
+import frontend.lexer.TokenListIterator;
+
+public class InitValParser {
+    private TokenListIterator iterator;
+
+    public InitValParser(TokenListIterator iterator) {
+        this.iterator = iterator;
+    }
+
+    public InitVal parseInitVal() {
+        /* TODO */
+        return null;
+    }
+}
