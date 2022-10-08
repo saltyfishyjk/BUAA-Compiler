950edfbd78b3117810ce97c03840ceeb6be3399c
YJK <yjk_official@foxmail.com>
2022-10-08 19:45:11 +0800

    feat: complete Error Class

--- homework/homework4_error_handler/src/middle/error/Error.java
+++ homework/homework4_error_handler/src/middle/error/Error.java
@@ -1,4 +1,26 @@
 package middle.error;
 
+/**
+ * 错误类
+ */
 public class Error {
+    private ErrorType type; // 所属错误类别
+    private int lineNum; // 从1开始
+
+    public Error(int lineNum, ErrorType type) {
+        this.lineNum = lineNum;
+        this.type = type;
+    }
+
+    public ErrorType getType() {
+        return type;
+    }
+
+    public int getLineNum() {
+        return lineNum;
+    }
+
+    public String getErrorTypeCode() {
+        return this.type.getCode();
+    }
 }
